e78ebf4a9fba36bd46a42e10018ede364e03a655
wangqing327 <794978483@qq.com>
2019-12-25 11:09:37 +0800

    导入导出

--- app/src/main/java/com/example/mypassword/MainActivity.java
+++ app/src/main/java/com/example/mypassword/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.mypassword;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -33,6 +35,13 @@ import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
@@ -42,7 +51,7 @@ import static android.text.Html.fromHtml;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button button;
-    long clicktime;
+    long ClickTime;
     final String TAG = "MainActivity";
     private static int selection_id = 0;//记住上次选中的ID
     SQLiteDatabase db;
@@ -53,7 +62,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private com.example.mypassword.RecyclerViewAdapter adapter;
     private ExRelativeLayout exRelativeLayout;
     private TextView textView, group1, group2, group3, group4, group5, group6;
-    private LinearLayout layout;
     private ImageView imageView;
 
     @Override
@@ -61,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initExRelativeLayout();
-        button = findViewById(R.id.button1);
+        button = findViewById(R.id.button_clear);
         button.setOnClickListener(this);
         edit = findViewById(R.id.ssk);
         edit.setSingleLine();//单行模式
@@ -187,9 +195,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //imageView.setAnimation(animation);
 
 
-            layout = findViewById(R.id.xsdw);
+            LinearLayout linearLayout = findViewById(R.id.xsdw);
 
-            layout.setOnClickListener(new View.OnClickListener() {
+            linearLayout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if (exRelativeLayout.isExpand()) {
@@ -198,6 +206,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         imageView.clearAnimation();
                         imageView.setAnimation(animation_top);
                         imageView.startAnimation(animation_top);
+
                     } else {
                         exRelativeLayout.setVisibility(View.INVISIBLE);
                         exRelativeLayout.expand();
@@ -205,6 +214,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         imageView.clearAnimation();
                         imageView.setAnimation(animation_bot);
                         imageView.startAnimation(animation_bot);
+
                     }
                     //执行动画
 
@@ -331,10 +341,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if ((System.currentTimeMillis() - clicktime) > 2000) {
+            if ((System.currentTimeMillis() - ClickTime) > 2000) {
                 Toast.makeText(getApplicationContext(), getResources().getString(R.string.isExit),
                         Toast.LENGTH_SHORT).show();
-                clicktime = System.currentTimeMillis();
+                ClickTime = System.currentTimeMillis();
             } else {
                 finish();
                 System.exit(0);
@@ -350,7 +360,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button1:
+            case R.id.button_clear:
                 edit.setText("");
                 edit.requestFocus();
                 v.setVisibility(View.GONE);
@@ -407,7 +417,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(TAG, "编辑框被单击");
                 edit.requestFocus();
                 break;
+
+            default:
+                break;
         }
+
+
     }
 
     public void rester() {
@@ -527,6 +542,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 openxiangqing(pos);
                 break;
             case R.id.edit:
+                if(list.size()<=0){
+                    break;
+                }
                 RecyclerViewAdapter.CarCaption carCaption = list.get(pos);
                 Intent intent = new Intent(this, AddDate.class);
                 intent.putExtra("id", carCaption.id);
@@ -535,7 +553,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivityForResult(intent, 1);
 
                 break;
+            case R.id.qianru:
+                //导入数据
+                inputDate();
+                break;
+            case R.id.qianchu:
+                //导出数据
+                outputDate();
 
+                break;
         }
         return true;
     }
@@ -560,6 +586,162 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         Toast.makeText(this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
         return true;
+    }
+
+    public void outputDate() {
+        String sql = getResources().getString(R.string.dcSql);
+        Cursor cursor = db.rawQuery(sql, null);
+        //select user,password,"group",url,carID,date,ls,ggrq,bz1,bz2,bz3 from date
+        if(cursor.getCount()==0){
+            cursor.close();
+            return;
+        }
+        String dcmm = MyAppModel.getDcmm();
+        try {
+            StringBuffer xml = new StringBuffer();
+            //  \t  Tab  \n空格
+            xml.append("<?xml version=\"1.0\" standalone=\"yes\"?>\n");
+            xml.append("<RECORDS>\n");
+            while (cursor.moveToNext()) {
+                xml.append("\t<RECORD>\n");
+                String user = cursor.getString(cursor.getColumnIndex("user"));
+                String pwd = cursor.getString(cursor.getColumnIndex("password"));
+                String url = cursor.getString(cursor.getColumnIndex("url"));
+                user = AES.decrypt(MyAppModel.getZpassword(), user);
+                user = AES.encrypt(dcmm, user);
+
+                pwd = AES.decrypt(MyAppModel.getZpassword(), pwd);
+                pwd = AES.encrypt(dcmm,pwd );
+
+                url = AES.decrypt(MyAppModel.getZpassword(), url);
+                url = AES.encrypt(dcmm, url);
+
+                xml.append("\t\t<user>" + user + "</user>\n");
+                xml.append("\t\t<pwd>" + pwd + "</pwd>\n");
+                xml.append("\t\t<url>" + url + "</url>\n");
+                xml.append("\t\t<group>" + cursor.getString(cursor.getColumnIndex("group")) + "</group>\n");
+                xml.append("\t\t<carID>" + cursor.getString(cursor.getColumnIndex("carID")) + "</carID>\n");
+                xml.append("\t\t<date>" + cursor.getString(cursor.getColumnIndex("date")) + "</date>\n");
+                xml.append("\t\t<ls>" + cursor.getString(cursor.getColumnIndex("ls")) + "</ls>\n");
+                xml.append("\t\t<ggrq>" + cursor.getString(cursor.getColumnIndex("ggrq")) + "</ggrq>\n");
+                xml.append("\t\t<bz1>" + cursor.getString(cursor.getColumnIndex("bz1")) + "</bz1>\n");
+                xml.append("\t\t<bz2>" + cursor.getString(cursor.getColumnIndex("bz2")) + "</bz2>\n");
+                xml.append("\t\t<bz3>" + cursor.getString(cursor.getColumnIndex("bz3")) + "</bz3>\n");
+                xml.append("\t</RECORD>\n");
+            }
+            cursor.close();
+            xml.append("</RECORDS>\n");
+            //getExternalCacheDir()
+            File xml_path = new File(Environment.getExternalStorageDirectory().getAbsoluteFile(), "MyPassWordDate/date.xml");
+           // File xml_path = new File(getExternalCacheDir(), "date.xml");
+            if(!xml_path.exists()){
+                File file = xml_path.getParentFile();
+                if(!file.exists()){
+                    file.mkdir();
+                }
+                xml_path.createNewFile();
+            }
+            FileOutputStream outputStream = new FileOutputStream(xml_path);
+            outputStream.write(xml.toString().getBytes());
+            xml.setLength(0);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void inputDate() {
+        try {
+
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            File xml_path = new File(Environment.getExternalStorageDirectory(), "MyPassWordDate/date.xml");
+            if(!xml_path.exists()){
+                MyAppModel.ShowToast(this,R.string.file_error);
+                return;
+            }
+            InputStream inputStream = new FileInputStream(xml_path);
+            xmlPullParser.setInput(inputStream, "UTF-8");
+            int eventType = xmlPullParser.getEventType();
+            //user, pwd, url, group, carID, date, ls, ggrq, bz1, bz2, bz3
+            String user = "", pwd = "", url = "", group = "", carID = "", date = "",
+                    ls = "", ggrq = "", bz1 = "", bz2 = "", bz3 = "";
+            int Sum = 0;
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        if ("user".equals(nodeName)) {
+                            user = xmlPullParser.nextText();
+                            user = AES.decrypt(MyAppModel.getDcmm(),user);
+                            user = AES.encrypt(MyAppModel.getZpassword(),user);
+                        } else if ("pwd".equals(nodeName)) {
+                            pwd = xmlPullParser.nextText();
+                            pwd = AES.decrypt(MyAppModel.getDcmm(),pwd);
+                            pwd = AES.encrypt(MyAppModel.getZpassword(),pwd);
+                        } else if ("url".equals(nodeName)) {
+                            url = xmlPullParser.nextText();
+                            url = AES.decrypt(MyAppModel.getDcmm(),url);
+                            url = AES.encrypt(MyAppModel.getZpassword(),url);
+                        } else if ("group".equals(nodeName)) {
+                            group = xmlPullParser.nextText();
+                        } else if ("carID".equals(nodeName)) {
+                            carID = xmlPullParser.nextText();
+                        } else if ("date".equals(nodeName)) {
+                            date = xmlPullParser.nextText();
+                        } else if ("ls".equals(nodeName)) {
+                            ls = xmlPullParser.nextText();
+                        } else if ("ggrq".equals(nodeName)) {
+                            ggrq = xmlPullParser.nextText();
+                        } else if ("bz1".equals(nodeName)) {
+                            bz1 = xmlPullParser.nextText();
+                        } else if ("bz2".equals(nodeName)) {
+                            bz2 = xmlPullParser.nextText();
+                        } else if ("bz3".equals(nodeName)) {
+                            bz3 = xmlPullParser.nextText();
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        ContentValues values = new ContentValues();
+                        values.put("user", user);
+                        values.put("password", pwd);
+                        values.put("url", url);
+                        values.put("\"group\"", group);
+                        values.put("carID", carID);
+                        values.put("date", date);
+                        values.put("ls", ls);
+                        values.put("ggrq", ggrq);
+                        values.put("bz1", bz1);
+                        values.put("bz2", bz2);
+                        values.put("bz3", bz3);
+                        if ("RECORD".equals(nodeName)) {
+                            long id = db.insert("date", null, values);
+                                if(id>0){
+                                    Sum++;
+                                }
+                        }
+                        values.clear();
+                        break;
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+            if ( Sum <= 0) {
+                MyAppModel.ShowToast(this, R.string.drsb);
+            } else {
+                MyAppModel.ShowToast(this, R.string.drcg);
+                //更新列表数据
+                list.clear();
+                addDate();
+                adapter.thlist(list);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
